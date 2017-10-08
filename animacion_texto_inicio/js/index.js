d27eeee40dbedbee10280205f4bf63d708e2817f
Constanza Morales <coniconon@users.noreply.github.com>
2017-10-08 19:51:31 -0300

    Animación de temblor con incremento

---
+++ animacion_texto_inicio/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	var count = 0;
+
+	function addone(){
+		count++;
+		$('#logo').jrumble({
+			x: count,
+			y: count,
+			rotation: 0
+		});
+		$('#logo').trigger('startRumble');
+
+		setTimeout(addone, 2000);
+	}
+	addone();
+});
+
