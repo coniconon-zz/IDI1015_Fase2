f5f668268a140ccef80fe0c718d5ee37130bc49a
Diego González Gibson <diego@MacBook-Air-de-Diego.local>
2017-10-12 16:34:35 -0300

    filtro arreglado

--- definitivo/js/mapa.js
+++ definitivo/js/mapa.js
@@ -1,11 +1,14 @@
+var layers_array = [];
+
+
 var map = L.map('map').setView([-21.351441,-71.7764874], 5);
 
 var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>contributors, &copy; <a href="http://carto.com/attributions">CARTO</a>'
     }).addTo(map);
 
-var scale = d3.scaleLinear().domain([0, 316000]).range([5,20]);
-var color = d3.scaleLinear().domain([1.6,9.5])
+var scale = d3.scaleLinear().domain([0, 316000]).range([10,20]);
+var color = d3.scaleLinear().domain([4.9,9.5])
       .interpolate(d3.interpolateHcl)
       .range([d3.rgb("#fffc33"), d3.rgb('#FF2700')]);
 
@@ -27,14 +30,12 @@ function onEachFeature(feature, layer) {
             feature.properties.DAMAGE +
   "</p>";
   layer.bindPopup(content);
-};
-
-
+}
 
 
 
 function display_by_year(year){
-  L.geoJSON(data, {
+        var point =  L.geoJSON(data, {
         pointToLayer: function (feature, latlng) {
         if (feature.properties.YEAR == year){
           if (feature.properties.DEATHS != "sin dato")
@@ -49,32 +50,48 @@ function display_by_year(year){
             fillColor: color(feature.properties.MAGNITUDE), fillOpacity: 0.8});
         }};
      }, onEachFeature: onEachFeature
-
    }).addTo(map);
-};
+
+
+   layers_array.push(point);
+   if (layers_array.length > 1){
+     console.log(layers_array);
+     map.removeLayer(layers_array[layers_array.length - 2]);
+   }
+}
 
 function clear() {
   L.geoJSON(data,{
     onEachFeature : function (feature, layer){
-      var group = L.layerGroup();
-      group.addLayer(layer);
-      group.eachLayer(function(layer2){
-      layer2.removeFrom(map)
-      });
-    }
-  });
-};
-function clear2(){
+      console.log(layer.latlng);
+    }});
+  }
+function clear2(year){
   L.geoJSON(data, {
-    onEachFeature: function(feature, layer){
-      layer.removeFrom(map)
-    }
-  });
-};
+          pointToLayer: function (feature, latlng) {
+          if (feature.properties.YEAR != year){
+            if (feature.properties.DEATHS != "sin dato")
+            {
+            return L.circleMarker(latlng, {radius:
+              scale(feature.properties.DEATHS),
+              stroke: false, fillColor: color(feature.properties.MAGNITUDE),
+              fillOpacity: 0.8});
+          }
+          else {
+            return L.circleMarker(latlng, {radius: 4, stroke: false,
+              fillColor: color(feature.properties.MAGNITUDE), fillOpacity: 0.8});
+          }};
+       }, onEachFeature: onEachFeature
+
+     }).removeFrom(map);
+  }
+
+
 function update(val){
+  // clear(val)
   console.log(val);
   document.getElementById("year-value").innerHTML = val;
   display_by_year(val);
 
 
-};
+}
